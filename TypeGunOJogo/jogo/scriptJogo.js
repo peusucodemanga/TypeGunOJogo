ab318b80649147af20ad6afe7955c04ae9ca2301
danielfaranidcomp <daniel.defarias@dcomp.ufs.br>
2024-10-15 21:18:11 -0300

    dividi cada palavra da frase numa span diferente

--- TypeGunOJogo/jogo/scriptJogo.js
+++ TypeGunOJogo/jogo/scriptJogo.js
@@ -3,15 +3,19 @@ const inputConteudo = document.getElementById('entradaTexto')
 
 const listaDeFrases = ['Depois que eu conheci o Mandela', 'Depois que eu vi como ela dança', 
     'Depois que eu vi como ela se assanha', 'Só agora que eu vim perceber', 'Namorar pra quê?',
-    'Se amarrar pra quê?', 'Prefiro estar solteiro que eu sei que elas vão querer']
+    'Se amarrar pra quê?', 'Prefiro estar solteiro que eu sei que elas \nvão querer']
 
 const sortearFrase = (max = -1, min = 7) => Math.floor(Math.random() * (max - min + 1)) + min
 
 
 function gerarNovaFrase() {
     const frase = listaDeFrases[sortearFrase()]
-    displayFrase.innerText = frase
-
+    displayFrase.innerHTML = ''
+    frase.split(' ').map((palavra) => {
+        const spanPalavra = document.createElement('span')
+        spanPalavra.innerText = `${palavra} `
+        displayFrase.appendChild(spanPalavra)
+    })
     inputConteudo.value = null
 }
 
